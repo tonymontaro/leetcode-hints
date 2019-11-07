7c1e5d17ceebdd9963a909600b9000ba68c50827
Anthony Ngene <ngeneanthony@gmail.com>
2019-11-07 10:50:53 +0100

    add  encode-and-decode-tinyurl solution

--- problems/encode-and-decode-tinyurl/encode_and_decode_tinyurl.py
+++ problems/encode-and-decode-tinyurl/encode_and_decode_tinyurl.py
@@ -1,23 +1,29 @@
-#!/usr/bin/env python
+import string
+import random
 
 class Codec:
+    def __init__(self):
+        self.urls = {}
+        self.known_urls = {}
+        self.characters = list(string.ascii_letters) + list('1234567890')
 
-    def encode(self, longUrl):
-        """Encodes a URL to a shortened URL.
-        
-        :type longUrl: str
-        :rtype: str
-        """
-        
-
-    def decode(self, shortUrl):
-        """Decodes a shortened URL to its original URL.
-        
-        :type shortUrl: str
-        :rtype: str
-        """
+    def encode(self, long_url):
+        if long_url in self.known_urls:
+            return self.known_urls[long_url]
+        short_url = None
+        while not short_url:
+            url = self.get_short_str()
+            if url not in self.urls:
+                short_url = url
+                self.urls[short_url] = long_url
+                self.known_urls[long_url] = short_url
+        return short_url
         
 
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(url))
+    def decode(self, short_url):
+        if short_url in self.urls:
+            return self.urls[short_url]
+
+    def get_short_str(self):
+        rs = [random.choice(self.characters) for _ in range(6)]
+        return ''.join(rs)
