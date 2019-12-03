fa172242b345176bfe106d96ba2b7d3db14691eb
Anthony Ngene <ngeneanthony@gmail.com>
2019-12-03 07:13:41 +0100

    update  counting-bits solution

--- problems/counting-bits/solution.py
+++ problems/counting-bits/solution.py
@@ -6,3 +6,14 @@ class Solution:
             result[i] = result[i//2]
             if i % 2 == 1: result[i] += 1
         return result
+
+    
+# Bit solution
+class Solution:
+    def countBits(self, num: int):
+        """O(n) time and space"""
+        result = [0] * (num + 1)
+        for i in range(1, num+1):
+            result[i] = result[i >> 1]
+            if i % 2 == 1: result[i] += 1
+        return result
