90eb9d5ad20c4eeff3e78a7487b29fa1ab209adc
Anthony Ngene <ngeneanthony@gmail.com>
2019-12-10 21:18:33 +0100

    Update solution.py

--- problems/house-robber/solution.py
+++ problems/house-robber/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def rob(self, nums):
+        """O(n) time and space"""
         result = [0] * len(nums)
         result[:2] = nums[:2]
         for i in range(2, len(nums)):
             num = nums[i]
             result[i] = max(num + result[i-2], num + result[i-1]-nums[i-1])
-        return max(result) if result else 0
+        return max(result[-2:]) if result else 0
