5e2bbcff720235837083ea5ce063d86b374a447d
Anthony Ngene <ngeneanthony@gmail.com>
2020-01-19 11:34:12 +0100

    Update solution.py

--- problems/subarray-sum-equals-k/solution.py
+++ problems/subarray-sum-equals-k/solution.py
@@ -12,3 +12,17 @@ class Solution:
             result += seen[sm - k]
             seen[sm] += 1
         return result
+
+    
+    ## No need for defaultdict
+    class Solution:
+    def subarraySum(self, nums, k: int) -> int:
+        """O(n) time and space"""
+        result = 0
+        sm = 0
+        seen = {0: 1}
+        for num in nums:
+            sm += num
+            result += seen.get(sm - k, 0)
+            seen[sm] = seen.get(sm, 0) + 1
+        return result
